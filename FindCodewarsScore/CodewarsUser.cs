2bbc23f5af67fa4fd199f2b4bafdbb4a91d0c447
Jake Watson <jakejakewatsonwatson@gmail.com>
2018-08-22 16:35:52 -0500

    large inital commit of a personal repository

---
+++ FindCodewarsScore/CodewarsUser.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+
+namespace FindCodewarsScore
+{
+    class CodewarsUser
+    {
+        public CodewarsUser(string username)
+        {
+            Username = username;
+        }
+
+        public string Username { get; private set; }
+        public string Name { get; set; }
+        public int Score { get; set; }
+
+
+        public static List<CodewarsUser> ParseUsers(string[] users)
+        {
+            if (users == null)
+                return new List<CodewarsUser>() { new CodewarsUser("N/A") { Name = "None", Score = 0 } };
+
+            var userList = new List<CodewarsUser>();
+
+            foreach (var user in users)
+            {
+                var data = user.Split(',');
+                var newUser = new CodewarsUser(data[0]);
+                newUser.Name = data[1].Trim();
+                userList.Add(newUser);
+            }
+
+            return userList;
+        }
+    }
+}
---
+++ FindCodewarsScore/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FindCodewarsScore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dataFinder = new DataFinder();
+
+            Console.WriteLine("Getting users...");
+            var usernames = dataFinder.GetUsernames("Usernames.csv");
+            if (usernames == null)
+                Environment.Exit(-1);
+
+            var codewarsUsers = CodewarsUser.ParseUsers(usernames);
+            if (codewarsUsers == null || codewarsUsers.Count < 1)
+                Environment.Exit(-1);
+
+            Console.WriteLine("Getting data from Codewars...");
+            dataFinder.PopulateScores(codewarsUsers);
+
+            codewarsUsers = codewarsUsers.OrderByDescending(x => x.Score).ToList();
+
+            Console.Clear();
+            Console.WriteLine(Formatter.GetTextFormattedForDisplay(codewarsUsers));
+            Console.ReadLine();
+        }
+    }
+}
